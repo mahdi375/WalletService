638d3de654701b2be42b91558137495179fb754d
Mahdi Ghasemi <ghasemim875@gmail.com>
2023-12-24 22:06:33 +0330

    min: imrove api urls

--- routes/v1/api.php
+++ routes/v1/api.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('ping', fn () => ['ping' => 'pong'])->name('health');
 
-Route::prefix('transaction')->name('transaction.')->group(function () {
+Route::prefix('transactions')->name('transaction.')->group(function () {
     Route::post('/', [TransactionController::class, 'store'])->name('store');
 });
 
-Route::prefix('wallet')->name('wallet.')->group(function () {
+Route::prefix('wallets')->name('wallet.')->group(function () {
     Route::get('{userId}/balance', [WalletController::class, 'getBalance'])->name('get-balance');
 });
