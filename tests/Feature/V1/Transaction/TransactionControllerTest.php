7c7c16ba0e021f0a309f47a5fd3ee5da4215fd38
Mahdi Ghasemi <ghasemim875@gmail.com>
2023-12-24 20:40:24 +0330

    test: add more transaction controller tests

--- tests/Feature/V1/Transaction/TransactionControllerTest.php
+++ tests/Feature/V1/Transaction/TransactionControllerTest.php
@@ -5,6 +5,7 @@ use App\Models\Wallet;
 use Illuminate\Testing\Fluent\AssertableJson;
 
 use function Pest\Laravel\assertDatabaseCount;
+use function Pest\Laravel\assertDatabaseEmpty;
 use function Pest\Laravel\assertDatabaseHas;
 use function Pest\Laravel\postJson;
 
@@ -14,7 +15,7 @@ it('can make a transaction', function () {
 
     $payload = [
         'user_id' => $wallet->user_id,
-        'amount' => $amount
+        'amount' => $amount,
     ];
 
     assertDatabaseCount(Transaction::class, 0);
@@ -29,4 +30,43 @@ it('can make a transaction', function () {
         'user_id' => $wallet->user_id,
         'amount' => $amount,
     ]);
-})->only();
+});
+
+it('adds ammount to wallet balance', function () {
+    $wallet = Wallet::factory()->create();
+    $amount = fake()->numberBetween(-100, 100);
+
+    $payload = [
+        'user_id' => $wallet->user_id,
+        'amount' => $amount,
+    ];
+
+    postJson(route('api.v1.transaction.store'), $payload)
+        ->assertSuccessful();
+
+    assertDatabaseHas(Wallet::class, [
+        'user_id' => $wallet->user_id,
+        'balance' => $wallet->balance + $amount,
+    ]);
+});
+
+it('will create a wallet if it does not exist', function () {
+    $amount = fake()->numberBetween(-100, 100);
+    $userId = fake()->numberBetween(1, 100);
+
+    $payload = [
+        'user_id' => $userId,
+        'amount' => $amount,
+    ];
+
+    assertDatabaseEmpty(Wallet::class);
+
+    postJson(route('api.v1.transaction.store'), $payload)
+        ->assertSuccessful();
+
+    assertDatabaseCount(Wallet::class, 1);
+    assertDatabaseHas(Wallet::class, [
+        'user_id' => $userId,
+        'balance' => $amount,
+    ]);
+});
